e5f25b325a98d533bc6e987de1970a8689314918
Nayana Loivos <loivosdev@gmail.com>
2023-11-20 15:56:56 -0300

    data type in Ruby

---
+++ 0-dataType.rb
@@ -0,0 +1,51 @@
+#Em Ruby tudo é um objeto, se tudo é um objeto, podemos afirmar
+#que todos os tipos possuem métodos e atribuitos que podem ser
+#envocados por qualquer elemento. Por exemplo: 3.times {puts 1}
+#Um tipo de dado define um conjunto de valores que uma variável pode 
+#armazenar e o conjunto de operações que pode ser executado com essa variável. 
+
+#Data Type
+
+#String
+word = "Nay"
+puts word.class
+#String é o que está entre aspas duplas ou simples.
+    #Qual a diferença?
+        #É na concatenação de string, onde se temos aspas duplas
+        #conseguimos concatenar assim: "#{}" e se temoa aspas 
+        #simples concatenamos assim: "..." + word.
+
+#Char
+letter = word[0]
+puts letter
+puts letter.class
+
+#Numeric
+    #Fixnum/Integer, Float herdam da class Numeric.
+    #Fixnum/Integer
+        #1, 2, 3, 4, 5, 6, ...
+
+    #Float
+        #1.2, 3.5, 4.0, ...
+
+#Nilclass
+null = nil
+puts null.class
+
+#Hash
+h = {}
+puts h.class
+
+#Array
+a = []
+puts a.class
+
+#Symbol
+    #É possível dispor de símbolos representativos
+    name_string = {"name": "Nayana"} #Hash com String
+    name_symbol = {:name => "Nayana"} #Hash com Symbol
+
+    puts nome_string["name"] #nao ta funcionando
+    puts nome_symbol[:name]
+
+#PS: .class pode ser digitado sobre qualquer variável e saber e qual tipo ela é.
---
+++ 6-string-management.rb
@@ -0,0 +1,12 @@
+#String é um espaço de memória ocupado no computador que tem uma variável do tipo string
+#Uma string é um valor, nomralmente escrito entre aspas duplas ou simples e pode possuir
+#letras e/ou números, ou seja, qualquer carater, texto ou frase é uma string.
+
+#match
+    name = "Nayana1234"
+    puts name.match(\D)
+
+    #por meio de uma expressão regular, podemos pegar somente os valores numeros da string.
+
+
+
