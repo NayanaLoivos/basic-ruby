3ac585eed2f55e421b2ae8821686fa0966cbdff1
Nayana Loivos <loivosdev@gmail.com>
2023-11-26 10:12:01 -0300

    heritages and the data types in ruby

---
+++ 3-heritagesAndTheDataTypes.rb
@@ -0,0 +1,38 @@
+#Dentro de Ruby o Object é o último tipo de herança de uma classe.
+#Ruby possue uma cadeia de herança onde a classe abaixo herda da classe acima.
+#
+  #String
+  word = "oi"
+  puts word.class #String
+  puts word.class.superclass #Object
+
+  #Integer
+  integer = 1
+  puts integer.class #Integer
+  puts integer.class.superclass #Numeric
+  puts integer.class.superclass.superclass #Object
+
+  #Float
+  float = 1.0
+  puts float.class #Foat
+  puts float.class.superclass #Numeric
+  puts float.class.superclass.superclass #Object
+
+  #Nil
+  null = nil
+  puts null.class #NilClass
+  puts null.class.superclass #Object
+
+  #Range
+  range = (1...8)
+  puts range.class #Range
+  puts range.class.superclass #Object
+  puts range.class.superclass.superclass #BasicObject
+  puts range.class.superclass.superclass.superclass #'impressão vazia'
+
+  #Hierarquia das classes em Ruby
+    #Essa hierarquia corresponde a uma cadeia de heranças as quais passam seus métodos e atributos a outras classes.
+    #BasicObject
+    #Object
+    #Demais classes
+
